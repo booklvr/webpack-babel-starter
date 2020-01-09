032a614789536dd94581dbf569a0f791d785eb08
nick <nick.a.dewaal@gmail.com>
2020-01-09 16:37:10 +0900

    adding final crud routes

--- src/index.js
+++ src/index.js
@@ -3,16 +3,34 @@
 import 'dotenv/config';
 import cors from 'cors'; //creates a cors header to prevent "Cross-Origin Request Blocked errors"
 import express from 'express';  //Creates Express Server
+import uuidv4 from 'uuid/v4';
 
 import  { hello } from './my-other-file';
 import { users, messages } from './data';
 
-
 const app = express();
+
+app.use(express.json()); // body-parser - parses incoming request stream and makes it accessible on req.body and exposes it as json
+app.use(express.urlencoded({ extended: true }));  // parses the text as url encoded data (how browsers tend to send form data) and exposes resulting object of keys and values on req.body
 
 app.use(cors()); // add CORS HTTP header to every request by default
 
+// MIDDLEWARE
+// * middleware comes first
+app.use((req, res, next) => {
+    // do something
+    req.me = users[1];  // pseudo authenticated user
 
+
+    next();
+})
+
+// SESSION
+app.get('/session', (req, res) => {
+    return res.send(users[req.me.id]);
+})
+
+// HOMEPAGE
 app.get('/', (req, res) => {
     return res.send('Received a GET HTTP method');
 });
@@ -20,7 +38,7 @@ app.get('/', (req, res) => {
 // app.get('/test', (req, res) => {
 //     res.send('This is a test');
 // })
-// HOMEPAGE
+
 app.post('/', (req, res) => {
    return res.send('Received a POST HTTP method');
 });
@@ -50,6 +68,40 @@ app.put('/users/:Id', (req, res) => {
 app.delete('/users/:userId', (req, res) => {
     return res.send(`DELETE HTTP methodon on user/${req.params.userId} resource`);
 });
+
+// MESSAGES
+app.get('/messages', (req, res) => {
+    return res.send(Object.values(messages));
+});
+
+app.post('/messages', (req, res) => {
+    const id = uuidv4();
+    const message = {
+        id,
+        text: req.body.text,
+        userId: req.me.id
+    };
+    console.log(req.me);
+
+    messages[id] = message;
+
+    return res.send(message);
+});
+
+app.put('/users/:Id', (req, res) => {
+    return res.send(`PUT HTTP methodon on user/${req.params.userId} resource`);
+});
+
+app.delete('/messages/:messageId', (req, res) => {
+    const {
+        [req.params.messageId]: message,
+        ...otherMessages
+    } = messages;
+
+    return res.send(message);
+});
+
+
 
 // console.log(users);
 
